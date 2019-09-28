d37bad92046edf25e3d0cee5454751b09c61b277
李慧吟 <>
2019-09-28 21:30:53 +0800

    提交

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="home">
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <div>{{infoModal}}</div>
+    </div>
+</template>
+<script>
+import {
+    mapState,
+    mapGetters,
+    mapMutations,
+    mapActions,
+} from 'vuex'
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+    name: 'home',
+    computed: {
+        ...mapState(['map']),
+        ...mapGetters(['infoModal']),
+    },
+    components: {
+        HelloWorld
+    }
+}
+</script>
